d597af281e9a249db8441b9b04b3e5b412d43118
27DH <lee_jackson@sina.cn>
2018-02-09 02:10:45 +0800

    2nd test

--- app/src/main/java/com/dailyart/daggerapplicationtestversion2/MainActivity.java
+++ app/src/main/java/com/dailyart/daggerapplicationtestversion2/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.dailyart.daggerapplicationtestversion2.secondtest.Shoe;
+
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     @Inject
     Cloth cloth;
+
+    @Inject
+    Shoe shoe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.myTextView);
         MainComponent build = DaggerMainComponent.builder().mainModule(new MainModule()).build();
         build.inject(this);
-        textView.setText(cloth.toString());
+        textView.setText(shoe.toString());
 
 
     }
---
+++ app/src/main/java/com/dailyart/daggerapplicationtestversion2/secondtest/Shoe.java
@@ -0,0 +1,19 @@
+package com.dailyart.daggerapplicationtestversion2.secondtest;
+
+import javax.inject.Inject;
+
+/**
+ * Created by 27DH on 2018/2/9.
+ */
+
+public class Shoe {
+    @Inject
+    public Shoe(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "鞋子";
+    }
+}
